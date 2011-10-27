ae686afd663d001e67ef2968b1c3763c632f2526
Matthew Smith <matt@m0smith.com>
2011-10-27 14:23:45 -0600

    initial revision

---
+++ src/topoged_hibernate/core.clj
@@ -0,0 +1,1 @@
+(ns topoged-hibernate.core)
---
+++ test/topoged_hibernate/test/core.clj
@@ -0,0 +1,6 @@
+(ns topoged-hibernate.test.core
+  (:use [topoged-hibernate.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
