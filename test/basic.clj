4f456c45ba65d1b4e456ab15ae88ed2308f25f12
Matthew Smith <matt@m0smith.com>
2011-10-28 13:01:17 -0600

    cleanup test code

--- test/basic.clj
+++ test/basic.clj
@@ -10,26 +10,29 @@
   (let [newmap (reduce conj m (map (fn [[k v]] [(name k) v]) m))]
     (java.util.HashMap. newmap)))
 
+
+(deftest list-in-tx []
+	 (with-hibernate-tx [session _]
+	   (let [events (.. session (createQuery "from Event") list)]
+	     (is (= 2 (count events))))))
+
+(deftest list-as-seq []
+	 (let [events (with-hibernate-tx [session _] (.. session (createQuery "from Event") list))]
+	   (is (= 2 (count events)))))
+
+
 (deftest basic-hibernate []
-	 (with-hibernate-tx [session   tx  ]
+	 (with-hibernate-tx [session tx]
 	     (.save session "Event" (wrap {:title "Our very first event!"
 					   :date (java.util.Date.)}))
 	     (.save session "Event" (wrap {:title "A follow-up event"
 					   :date (java.util.Date.)})))
-	 (with-hibernate-tx [session _]
-	   (let [rows (.. session (createQuery "from Event") list)]
-	     (is (= 2 (count rows)))
-	     (println rows))))
-
-(deftest lazy-results []
-	 (println 
-	  (with-hibernate-tx [session _]
-	    (let [rows (.. session (createQuery "from Event") list)]
-	      rows))))
+	 (list-in-tx)
+	 (list-as-seq))
 
 
-
-
+(defn test-ns-hook []
+  (basic-hibernate))
 
 
 
